beae0529dc0ff390371794044d7f934fd878b0e2
Vasya Makarchuk <vasyamakarchuk@outlook.com>
2016-11-29 14:05:21 +0300

    Add project files.

---
+++ chat_server.BLL/Implementations/ClientObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+
+namespace chat_server.BLL.Implementations
+{
+    public class ClientObject
+    {
+        protected internal string Id { get; private set; }
+        protected internal NetworkStream Stream { get; private set; }
+    }
+}
