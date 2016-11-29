21d0522e32f51ae5470b3f965a9e4feb083c85c9
Vasya Makarchuk <vasyamakarchuk@outlook.com>
2016-11-29 14:53:10 +0300

    server logic finish

--- chat_server.BLL/Implementations/ClientObject.cs
+++ chat_server.BLL/Implementations/ClientObject.cs
@@ -27,17 +27,68 @@ namespace chat_server.BLL.Implementations
 
         public void Process()
         {
-            throw new NotImplementedException();
+            try
+            {
+                Stream = client.GetStream();
+                string message = GetMessage();
+                userName = message;
+
+                message = userName + " enter to the chat!";
+                _serverObject.BroadcastMessage(message, this.Id);
+                Console.WriteLine(message);
+
+                while (true)
+                {
+                    try
+                    {
+                        message = GetMessage();
+                        message = String.Format("{0}: {1}", userName, message);
+                        Console.WriteLine(message);
+                        _serverObject.BroadcastMessage(message, this.Id);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        message = String.Format("{0}: покинул чат", userName);
+                        Console.WriteLine(message);
+                        _serverObject.BroadcastMessage(message, this.Id);
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                _serverObject.RemoveConnection(this.Id);
+                Close();
+            }
         }
 
         public string GetMessage()
         {
-            throw new NotImplementedException();
+            byte[] data = new byte[256];
+            StringBuilder builder = new StringBuilder();
+            int bytes = 0;
+
+            do
+            {
+                bytes = Stream.Read(data, 0, data.Length);
+                builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+            }
+            while (Stream.DataAvailable);
+
+            return builder.ToString();
         }
 
         public void Close()
         {
-            throw new NotImplementedException();
+            if (Stream != null)
+                Stream.Close();
+            if (client != null)
+                client.Close();
         }
     }
 }
